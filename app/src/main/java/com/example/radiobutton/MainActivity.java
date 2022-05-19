f61f0d369e2f385bc9e58a14d5b2864760e51540
omi-72 <jannat.omi722@gmail.com>
2022-05-19 23:47:39 +0600

    call id on mainActivity

--- app/src/main/java/com/example/radiobutton/MainActivity.java
+++ app/src/main/java/com/example/radiobutton/MainActivity.java
@@ -3,8 +3,20 @@ package com.example.radiobutton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText userName;
+    RadioGroup rd_group;
+    RadioButton rd_btn_male, rd_btn_female;
+    CheckBox chk_terms;
+
+    Button btn_submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
